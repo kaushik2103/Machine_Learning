29030b633c58699a15a0f7fd26f42be1c736a1db
Dhruv Tillu <tillu3196@gmail.com>
2024-02-06 14:38:43 +0530

    Automated EDA Updated

--- Automated_EDA/main.py
+++ Automated_EDA/main.py
@@ -1,7 +1,16 @@
 import seaborn as sns
+import pandas as pd
 
 df = sns.load_dataset('titanic')
-import sweetviz as sv
 
+# Using SweetViz Library
+import sweetviz as sv
 my_report = sv.analyze(df)
 my_report.show_html()
+
+# Using dtale Library
+import dtale
+import dtale.app as dtale_app
+
+dtale_app.USE_COLAB = True
+dtale.show(df)
